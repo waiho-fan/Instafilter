e0a558164c1f8ae032bfc92618d0b0068290b7b2
Gary Fan <tomfan419@gmail.com>
2025-01-04 01:03:48 +0800

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Gary on 4/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
